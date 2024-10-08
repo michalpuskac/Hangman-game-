60af5f5d5efefb3afc47cf281c37ae53d2d0a042
Michal Puškáč <133041741+michalpuskac@users.noreply.github.com>
2024-10-08 13:13:33 +0200

    Add pseudocode and todos

---
+++ first_hangman.py
@@ -0,0 +1,23 @@
+#TODO Import modules.
+
+#TODO Show game welcomes and logos.
+
+#TODO Show menu, with game styles and difficulty.
+
+#TODO Pick a random word from module.
+
+#TODO Show underscore for each letter of the word.
+
+#TODO Ask user for input.
+
+#TODO Check lenght of input.
+
+#TODO Validate user guess if it's right or wrong. Wrong means lives -1.
+
+#TODO Check repetitive guesses.
+
+#TODO Clear screen after each guess to avoid full terminal of history.
+
+#TODO Define if user is Winner or Loser.
+
+#TODO Ask to play again or quit.
